24fa7ad9f62f6d4122184d4c0ac97bc559413073
v-ops <workmailvicky101@gmail.com>
2023-09-06 19:27:25 +0530

    initial commit

---
+++ seleniumSessions/WebDriverBasics.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+
+"""Seleniu python Script with headless Chrome"""
